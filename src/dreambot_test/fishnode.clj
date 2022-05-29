d5a81d48670512639f47c1365c9f0b6aac6e793e
EricEhmke <eric@ericehmke.com>
2022-05-29 11:50:25 -0400

    Add ability to fish from spot

--- src/dreambot_test/fishnode.clj
+++ src/dreambot_test/fishnode.clj
@@ -4,44 +4,50 @@
  [org.dreambot.api.methods.container.impl Inventory]
  [org.dreambot.api.methods.map Area]
  [org.dreambot.api Client]
- [org.dreambot.api.methods MethodProvider])
+ [org.dreambot.api.methods.interactive NPCs]
+ [org.dreambot.api.wrappers.interactive NPC]
+ [org.dreambot.api.methods MethodProvider]
+ [org.dreambot.api.methods.filter Filter])
 
 (defn isAtFishingZone
   "Checks whether the player is in the fishing zone"
   []
-  ;; Catherby
-  ;; (def catherbyFishing (new Area 2833 3436 2861 3421))
-  ;; (def inArea (.contains catherbyFishing (.getLocalPlayer Client)))
-  ;; TODO: Move the area & defintion to top level and pass in if this works OK
-  ;; (let [atLocation (.contains (new Area 2833 3436 2861 3421) #(Client/getLocalPlayer))]
-  ;;   (MethodProvider/log (str "at location " atLocation))
-  ;;   atLocation)
-  true)
+  (.contains (new Area 2833 3436 2861 3421) (Client/getLocalPlayer)))
 
 (defn hasInventorySpace
-  "Checks whether the players inventory is full"
+  "Makes sure there is space in the player's inventory"
   []
-  ;; (let [inventoryFull #(Inventory/isFull)]
-  ;;   (MethodProvider/log (str "Is inventory full" inventoryFull))
-  ;;   inventoryFull)
-  (boolean true))
+  (not (Inventory/isFull)))
 
 (defn hasRequiredTool
   "Checks whether that player has the required tool"
   []
-  ;; (let [hasTool (#(Inventory/contains "Lobster pot"))]
-  ;;   (MethodProvider/log (str "Has tool " hasTool))
-  ;;   hasTool)
-  (boolean true))
+  (Inventory/contains "Lobster pot"))
 
 (defn goFishing
   "Fishs"
   []
-  (let [fishingSpot (.. #(Client/getNPCs) .closest (filter (fn [spot] (.hasAction spot "Cage"))))]
-    (if-not (nil? fishingSpot)
-      (.interact fishingSpot "Cage")
-      (MethodProvider/log "There was a problem interacting with the fishing spot")))
-  (MethodProvider/sleepUntil (not (.isAnimating #(Client/getLocalPlayer))) 50000)
+  ;; (let [fishingSpot (#(Client/getNPCs) (filter (fn [spot] (.hasAction spot "Cage"))))]
+  ;;   (if-not (nil? fishingSpot)
+  ;;     (.interact fishingSpot "Cage")
+  ;;     (MethodProvider/log "There was a problem interacting with the fishing spot")))
+  ;; (let [fishingSpot  #(NPCs/closest (fn [x] (.hasAction x "Cage")))]
+  ;;   (MethodProvider/log (str "Found fishing spot " fishingSpot))
+  ;;   (if-not (nil? fishingSpot)
+  ;;     (.interact #(fishingSpot) (:action "Cage"))
+  ;; (.interact #(NPCs/closest (fn [x] (.hasAction x "Cage"))))
+  (let [fishingSpot  (NPCs/closest (into-array ["Fishing spot"]))]
+  ;; (let [fishingSpot  (.closest (Client/getNPCs) (fn [x] (.hasAction x "Cage")))]
+    (MethodProvider/log (str "Found fishing spot " fishingSpot))
+    (if (.interact fishingSpot "Cage")
+      (do
+        (MethodProvider/log "Fishing now")
+        (MethodProvider/sleepUntil (not (.isAnimating (Client/getLocalPlayer))) 50000))
+      (MethodProvider/log "Not fishing for some reason"))
+    ;; (if-not (nil? fishingSpot)
+    ;;   (.interact fishingSpot (:action "Cage"))
+    ;;   (MethodProvider/log "There was a problem interacting with the fishing spot"))
+    )
   (MethodProvider/sleep 5000))
 
 (defn FishNode
@@ -49,7 +55,6 @@
   (proxy [org.dreambot.api.script.TaskNode] []
     (priority [] (int 1))
     ;; Will return 'true' when these conditions are met
-    ;; (accept [] (MethodProvider/log "Eval Fishnode")  (MethodProvider/log (str "results " hasRequiredTool)) (boolean hasRequiredTool)) ;;TODO: Figure out why this is evaluating to false
-    (accept [] (MethodProvider/log "Eval Fishnode") (let [evalResults (every? true? [hasInventorySpace hasRequiredTool])] (MethodProvider/log (str "results " evalResults)) (boolean hasRequiredTool))) ;;TODO: Figure out why this is evaluating to false
+    (accept [] (MethodProvider/log "Evaluating whether to fish")  (and (hasInventorySpace) (hasRequiredTool) (isAtFishingZone)))
     ;; What will execute when this node runs.
     (execute [] (goFishing) (int 3000))))
