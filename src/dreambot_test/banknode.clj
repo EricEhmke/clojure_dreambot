ab5888b1877a41e0b0c3b2c806e434d0fd2584e9
Eric <eric@ericehmke.com>
2022-06-03 15:33:30 -0400

    Add banknode

--- src/dreambot_test/banknode.clj
+++ src/dreambot_test/banknode.clj
@@ -1,10 +1,19 @@
-(ns dreambot-test.banknode)
+(ns dreambot-test.banknode
+  (require [dreambot-test.core.utils.utilites :as utils]))
 
-(import [org.dreambot.api.methods.container.impl Inventory])
+(import
+ [org.dreambot.api.methods.container.impl Inventory]
+ [org.dreambot.api.methods MethodProvider]
+ [org.dreambot.api Client]
+ [org.dreambot.api.methods.container.impl.bank Bank]
+ [org.dreambot.api.utilities.impl Condition])
+
+(defn interactWithBank
+  [])
 
 (defn BankNode
   []
   (proxy [org.dreambot.api.script.TaskNode] []
     (priority [] (int 2))
-    (accept [] (Inventory/isFull))
-    (execute [] (Inventory/dropAll) (int 3000))))
+    (accept [] (MethodProvider/log "Evaluating whether to bank...") (Bank/isOpen))
+    (execute [] (interactWithBank) (int 3000))))
